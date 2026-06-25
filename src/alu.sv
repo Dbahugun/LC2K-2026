1171d3aad5840db2066756260bcbf8b19b159b5a
Dbahugun <dbahugun@umich.edu>
2026-06-24 23:51:26 -0400

    First commit of the project. Still needs work but a good start tonight. After this no more claude because the linter is finally working well and the ALU was the only structurally different thing.

---
+++ src/alu.sv
@@ -0,0 +1,38 @@
+//File name: alu.sv
+module alu
+    #(parameter W = 32)             // Default bit width
+    (
+        //input clk,
+        input logic [W-1:0] numberOne,
+        input logic [W-1:0] numberTwo,
+        input logic mode,
+        output logic [W-1:0] result,
+        output logic zeroFlag,
+        output logic equalFlag,
+        output logic overflow
+    );
+
+    //logic [W-1:0] resultBuff;
+    always_comb
+    begin
+        case (mode)
+            1'b0:
+                result = numberOne + numberTwo;
+            1'b1:
+                result = ~(numberOne | numberTwo);
+        endcase
+        zeroFlag = ~|(result);
+        equalFlag = (numberOne == numberTwo);
+        //Need to fix this because I'm using a new structure and have to revisit how to calculate overflow without carry signals. Perhaps more parenthesis?
+        overflow = (result[W-1] ^ numberOne[W-1] ^ numberTwo[W-1]);
+    end
+    //Potentially need to get rid of this. I think for multi-cycle this is needed but in reality the ALU combinationally will be fast enough/needs to be fast enough.
+    /*
+    always_ff @(posedge clk) 
+    begin
+        result <= resultBuff[W-1:0];
+    end
+    */
+
+
+endmodule
