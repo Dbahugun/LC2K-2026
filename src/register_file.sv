77b786013fa25108ddd7fd4c95e6a85c68358f40
Dbahugun <dbahugun@umich.edu>
2026-06-26 19:08:16 -0400

    Register file should be done, added synchronous reset so that I can initialize the registers to be 0 so that I can trust that tey won't be undefined at first

--- src/register_file.sv
+++ src/register_file.sv
@@ -1,6 +1,8 @@
 module register_file
     #(parameter W = 32)
     (
+        input clk,
+        input reset,
         input logic [2:0] addressA,
         input logic [2:0] addressB,
         input logic [2:0] addressDest,
@@ -11,6 +13,24 @@ module register_file
     );
 
     //Start writing implementation here for the actual register file. Should be an array of FFs, but how do I initialize them? Do I even need to?
-    
 
+    logic [7:0][W-1:0] registers;
+
+    always_comb 
+    begin
+        registerA = registers[addressA];
+        registerB = registers[addressB];
+    end
+
+    always_ff @( posedge clk ) 
+    begin
+        if(WrEn) begin
+            registers[addressDest] <= dataIn;
+        end
+        else if (reset) begin
+            for(int i = 0; i < 8; ++i) begin
+                registers[i] <= 'b0;
+            end
+        end
+    end
 endmodule
