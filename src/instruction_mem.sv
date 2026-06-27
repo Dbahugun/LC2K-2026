b3a14e4b8f83b4c284716b769be64d80df74819d
Dbahugun <dbahugun@umich.edu>
2026-06-27 14:47:10 -0400

    allegedly this is it for instruction memory. I don't feel super comfortable letting GOWIN handle it but its kind of out of my control

---
+++ src/instruction_mem.sv
@@ -0,0 +1,24 @@
+//IP Core Generator snippet goes below. Allegedly I chuck this into top.sv and never worry about it again
+
+//Copyright (C)2014-2026 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.12.02_SP2 (64-bit)
+//IP Version: 1.0
+//Part Number: GW2AR-LV18QN88PC8/I7
+//Device: GW2AR-18
+//Created Time: Sat Jun 27 14:39:58 2026
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    /*instruction_mem your_instance_name(
+        .dout(dout), //output [31:0] dout
+        .clk(clk), //input clk
+        .oce(oce), //input oce
+        .ce(ce), //input ce
+        .reset(reset), //input reset
+        .ad(ad) //input [7:0] ad
+    );*/
+
+//--------Copy end-------------------
