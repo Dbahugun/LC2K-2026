3710c9691ed4d3660d4459c27376611d77427057
Dbahugun <dbahugun@umich.edu>
2026-06-27 14:53:19 -0400

    So I fixed it now I missed the instruction_mem module, but it has errors which I've been assured is fine? I'm not sure but we'll have to table this for later I think

--- src/instruction_mem.sv
+++ src/instruction_mem.sv
@@ -1,24 +1,39 @@
-//IP Core Generator snippet goes below. Allegedly I chuck this into top.sv and never worry about it again
+//IP Core Generator snippet goes below. Allegedly I call this and never worry about it again, but there are errors?
 
 //Copyright (C)2014-2026 Gowin Semiconductor Corporation.
 //All rights reserved.
-//File Title: Template file for instantiation
+//File Title: IP file
 //Tool Version: V1.9.12.02_SP2 (64-bit)
 //IP Version: 1.0
 //Part Number: GW2AR-LV18QN88PC8/I7
 //Device: GW2AR-18
 //Created Time: Sat Jun 27 14:39:58 2026
 
-//Change the instance name and port connections to the signal names
-//--------Copy here to design--------
+module instruction_mem (dout, clk, oce, ce, reset, ad);
 
-    /*instruction_mem your_instance_name(
-        .dout(dout), //output [31:0] dout
-        .clk(clk), //input clk
-        .oce(oce), //input oce
-        .ce(ce), //input ce
-        .reset(reset), //input reset
-        .ad(ad) //input [7:0] ad
-    );*/
+output [31:0] dout;
+input clk;
+input oce;
+input ce;
+input reset;
+input [7:0] ad;
 
-//--------Copy end-------------------
+wire gw_gnd;
+
+assign gw_gnd = 1'b0;
+
+pROM prom_inst_0 (
+    .DO(dout[31:0]),
+    .CLK(clk),
+    .OCE(oce),
+    .CE(ce),
+    .RESET(reset),
+    .AD({gw_gnd,ad[7:0],gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd})
+);
+
+defparam prom_inst_0.READ_MODE = 1'b0;
+defparam prom_inst_0.BIT_WIDTH = 32;
+defparam prom_inst_0.RESET_MODE = "SYNC";
+
+endmodule //instruction_mem
+
