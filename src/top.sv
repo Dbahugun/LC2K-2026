91e88a3f4053145188c14505df0f342510c4578b
Dbahugun <dbahugun@umich.edu>
2026-07-11 16:05:07 -0400

    DPB configured BSRAM for data_mem is done, all I have to do now is control pROM and write/load the mi file before I can get cracking on top.sv

--- src/top.sv
+++ src/top.sv
@@ -1,4 +1,4 @@
-//Instruction mem initialization goes as follow
+//Instruction mem initialization goes as follows
 
 //Copyright (C)2014-2026 Gowin Semiconductor Corporation.
 //All rights reserved.
@@ -22,3 +22,40 @@
     );*/
 
 //--------Copy end-------------------
+
+//Data mem initialization goes as follows
+
+//Copyright (C)2014-2026 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.12.02_SP2 (64-bit)
+//IP Version: 1.0
+//Part Number: GW2AR-LV18QN88PC8/I7
+//Device: GW2AR-18
+//Created Time: Sat Jul 11 16:02:54 2026
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    /*data_mem your_instance_name(
+        .douta(douta), //output [31:0] douta
+        .doutb(doutb), //output [31:0] doutb
+        .clka(clka), //input clka
+        .ocea(ocea), //input ocea
+        .cea(cea), //input cea
+        .reseta(reseta), //input reseta
+        .wrea(wrea), //input wrea
+        .clkb(clkb), //input clkb
+        .oceb(oceb), //input oceb
+        .ceb(ceb), //input ceb
+        .resetb(resetb), //input resetb
+        .wreb(wreb), //input wreb
+        .ada(ada), //input [7:0] ada
+        .dina(dina), //input [31:0] dina
+        .adb(adb), //input [7:0] adb
+        .dinb(dinb) //input [31:0] dinb
+    );*/
+
+//--------Copy end-------------------
+
+
